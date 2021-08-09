fdb5eb511b87875f85a13a6c1ba1ecefc77b3998
Andrei Karotki <Andreikorotki@gmail.com>
2021-08-09 10:14:49 +0300

    feat: implement morse decoder

--- src/index.js
+++ src/index.js
@@ -39,8 +39,40 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    chanks = [];
+    var j = 0;
+    while ( j < expr.length){
+        endOfChank = j + 10;
+        chank = expr.slice(j, endOfChank);
+        chanks.push(chank);
+        j = endOfChank;
+    }
+
+    morsecode = []
+    for (let i = 0; i < chanks.length; i++) {
+        if (chanks[i] === '**********') {
+            morsecode.push(' ');
+        }
+        else {
+            morsecode.push(parseInt(chanks[i], 10).toString().replace(/10/g, '.').replace(/11/g, '-'));
+        }
+    }
+
+    decoded = '';
+    for(let i = 0; i < morsecode.length; i++) {
+        if (morsecode[i] === ' ') {
+            decoded = decoded + ' ';
+            continue;
+        }
+        else {
+            let letter = MORSE_TABLE[morsecode[i]];
+            decoded = decoded + letter;
+        }
+    }
+    return decoded;
 }
 
 module.exports = {
     decode
 }
+
